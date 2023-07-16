583999e019b054c0b65ca733cbf9744dcbe1b50a
Mehedi Hasan <Mohammadmehedi1250@gmail.com>
2023-07-17 02:10:48 +0600

    init

--- hudai.cpp
+++ hudai.cpp
@@ -1,36 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Person
+// Create class for the Object
+class Student
 {
 public:
-    string name;
-    float height;
-    int age;
-
-    Person(const string &personName, float personHeight, int personAge)
-        : name(personName), height(personHeight), age(personAge)
-    {
-    }
+    // Structure and behavior of the object
 };
 
 int main()
 {
-    Person person1("Mehedi Hasan", 180.5, 22);
-    Person person2("Monisha Akter Asha", 155.0, 20);
 
-    if (person1.age > person2.age)
-    {
-        cout << person1.name << "'s age is greater." << endl;
-    }
-    else if (person1.age < person2.age)
-    {
-        cout << person2.name << "'s age is greater." << endl;
-    }
-    else
-    {
-        cout << "Both persons have the same age." << endl;
-    }
+    Student Mehedi; // Declare of the obeject
+
+    //  Mehedi.
+    // Intialize value bassed on the class  Initialization or further operations on the object can be added here
 
     return 0;
 }
